5b68e130a93b3622a37249bc874e3064ceba2db5
MelidaSerpa <smelidaelizabeth@gmail.com>
2023-10-21 12:54:42 -0500

    Not much :/
    + On click button sound volume fixed.
    + DoNotDestroy script added (Needs to be fixed, details in "Issue" list in Trello).
    + Credit button added in Main menu, scene change configured.

---
+++ Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoNotDestroy : MonoBehaviour
+{
+	private void Awake()
+	{
+		GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+		GameObject[] gameObj = GameObject.FindGameObjectsWithTag("InGame");
+		
+        if (gameObj.Length > 0)
+        {
+            Debug.Log("In GameScene, destroying music object.");
+        }
+		if (musicObj.Length > 1)
+		{
+			Destroy(this.gameObject);
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+}
